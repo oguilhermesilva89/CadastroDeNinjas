65ca3a7b3353ac94935734de10e04e7ca395b9b1
oguilhermesilva89 <guilhermesallow@gmail.com>
2026-03-09 17:36:20 -0300

    Criado a controller [Aula2]

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
