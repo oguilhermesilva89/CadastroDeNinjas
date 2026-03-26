0941ad6aec4d0ebdef92f55dbbe4468991fce067
oguilhermesilva89 <guilhermesallow@gmail.com>
2026-03-26 07:37:23 -0300

    feat: Inclui o metodo Put porem não funciona corretamente, preciso trabalhar em alterações parciais

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -36,4 +36,13 @@ public class NinjaService {
     public void deletarNinjaPorId(Long id){
         ninjaRepository.deleteById(id);
     }
+
+    // Atualizar ninja
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado){
+        if(ninjaRepository.existsById(id)){
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 }
