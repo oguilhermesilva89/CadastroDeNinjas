cc0de13c386a7d96954e650cc161507c623e3f6a
oguilhermesilva89 <guilhermesallow@gmail.com>
2026-03-28 08:27:35 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,23 @@
+// DTO -  Data Transfer Object (Objeto de Transferência de Dados)
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String rank;
+    private int idade;
+    private MissoesModel missoes;
+
 }
